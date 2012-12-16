44fd3266241870512df2a3838a76a8ed35955145
CS User <csuser@cs352-22.cs.rutgers.edu>
2012-12-15 20:43:05 -0500

    setup sh

--- setup.sh
+++ setup.sh
@@ -2,25 +2,19 @@
 
 PATH=$PATH:/usr/sbin:/sbin
 
-
-if [ "$1" == "server" ]; then
-	IP_END=1
-elif [ "$1" == "client" ]; then
-	IP_END=2
-else
-	echo "RUN like this: $0 client for 10.2.2.2"
-	echo -e "\t $0 server for 10.2.2.1"
+if [ "$1" == "" ]; then
+	echo "RUN like this: $0 <IPEND> for 10.2.2.<IPEND>"
 	exit
 fi
 
 #Create tap devices on each machine by executing this command:
-sudo openvpn --mktun --dev tun2
+sudo openvpn --mktun --dev tap2 --lladdr "DE:AD:AC:BD:09:0$1"
  
 #The the kernel the interface is active/up:
-sudo ip link set tun2 up
+sudo ip link set tap2 up
  
 #Add an IP to the interface and tell the kernel to route packets matching the first 24 bits of the address to it:
-sudo ip addr add 10.2.2.${IP_END}/24 dev tun2
+sudo ip addr add 10.2.2.$1/24 dev tap2
  
 #Verify the interface is up and packets will be routed to it by looking at the routing table:
 echo -e "\nRouting to:\n\t`sudo route -n | grep tun2`"
